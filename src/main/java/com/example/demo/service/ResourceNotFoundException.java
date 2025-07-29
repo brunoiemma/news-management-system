23eaf0119d328d9937f758d130b2b13cf544831e
Bruno Iemma <bruno.iemma@globant.com>
2025-07-29 10:44:02 -0300

    Moved ResourceNotFoundException to service package

--- src/main/java/com/example/demo/service/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/service/ResourceNotFoundException.java
@@ -1,14 +1,12 @@
-package com.example.news.management.system.exception;
+package com.example.demo.service;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- * Excepción personalizada para indicar que un recurso no fue encontrado.
- * Al ser lanzada, Spring Boot responderá con un estado HTTP 404 (Not Found).
- */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(String message) {
         super(message);
